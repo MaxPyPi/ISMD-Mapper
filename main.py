75057c44e0b052347c7a0f4732f3c3468cb1bf7f
MaxPyPi <maxsulicn@gmail.com>
2025-06-10 21:41:15 +0900

    Tested new Database.py Also added testing reference to main.py
    Is ready for 0.0.2-full

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import Database
+from Database import Geological_Place
 
-
-Place = Database.Geological_Place()
+Place = Geological_Place()
 
 Place.ask_temp()
 Place.ask_cont()
-
+Place.sync_clock()
+print(Place.check_clock())
 Place.print_knowledge_check()
